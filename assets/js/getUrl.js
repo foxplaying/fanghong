f5a7017501c5c541525bdb0f166584eae8ca6759
foxplaying <166147707+foxplaying@users.noreply.github.com>
2024-04-08 23:22:59 +0800

    Create getUrl.js

---
+++ assets/js/getUrl.js
@@ -0,0 +1,30 @@
+function isValidUrl(text) {
+    return /\./.test(text);
+}
+function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        buildUrl();
+    }
+}
+function buildUrl() {
+    var targetUrl = document.getElementById('targetUrl').value;
+    if (!isValidUrl(targetUrl)) {
+        alert("请输入有效的网址");
+        return;
+    }
+    var apiUrl = 'http://jumps.test.upcdn.net/buildUrl.html?url=' + targetUrl);
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.msg === "成功") {
+                document.getElementById('generatedUrl').textContent = "生成的防红链接：" + data.short_url;
+            } else {
+                alert("生成失败");
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert("生成失败");
+        });
+}
