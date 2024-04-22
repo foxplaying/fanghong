d833b241011ae62290014b56d5bf3ff1cdc01dfe
foxplaying <166147707+foxplaying@users.noreply.github.com>
2024-04-23 02:13:16 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,63 +2,12 @@
     "use strict";
     var $body = document.querySelector('body');
 
-    !function() {
-        function t(t) {
-            this.el = t;
-            for (var n = t.className.replace(/^\s+|\s+$/g, "").split(/\s+/), i = 0; i < n.length; i++) e.call(this, n[i])
-        }
-        function n(t, n, i) {
-            Object.defineProperty ? Object.defineProperty(t, n, {
-                get: i
-            }) : t.__defineGetter__(n, i)
-        }
-        if (!("undefined" == typeof window.Element || "classList" in document.documentElement)) {
-            var i = Array.prototype,
-                e = i.push,
-                s = i.splice,
-                o = i.join;
-            t.prototype = {
-                add: function(t) {
-                    this.contains(t) || (e.call(this, t), this.el.className = this.toString())
-                },
-                contains: function(t) {
-                    return -1 != this.el.className.indexOf(t)
-                },
-                item: function(t) {
-                    return this[t] || null
-                },
-                remove: function(t) {
-                    if (this.contains(t)) {
-                        for (var n = 0; n < this.length && this[n] != t; n++);
-                        s.call(this, n, 1), this.el.className = this.toString()
-                    }
-                },
-                toString: function() {
-                    return o.call(this, " ")
-                },
-                toggle: function(t) {
-                    return this.contains(t) ? this.remove(t) : this.add(t), this.contains(t)
-                }
-            }
-            window.DOMTokenList = t, n(Element.prototype, "classList", function() {
-                return new t(this)
-            })
-        }
-    }();
-
     window.canUse = function(p) {
         if (!window._canUse) window._canUse = document.createElement("div");
         var e = window._canUse.style,
             up = p.charAt(0).toUpperCase() + p.slice(1);
-        return p in e || "Moz" + up in e || "Webkit" + up in e || "O" + up in e || "ms" + up in e
+        return p in e || "Moz" + up in e || "Webkit" + up in e || "O" + up in e || "ms" + up in e;
     };
-
-    (function() {
-        if ("addEventListener" in window) return;
-        window.addEventListener = function(type, f) {
-            window.attachEvent("on" + type, f)
-        }
-    })();
 
     window.addEventListener('load', function() {
         window.setTimeout(function() {
@@ -66,17 +15,47 @@
         }, 100);
     });
 
-    var settings = {
-        imageUrl: 'https://moe.jitsu.top/img/?sort=pc', 
-        delay: 6000 
-    };
+    (function() {
+        var settings = {
+            images: {
+                'https://moe.jitsu.top/img/?sort=pc': 'center'
+            },
+            delay: 6000
+        };
+        var pos = 0,
+            lastPos = 0,
+            $wrapper,
+            $bgs = [],
+            $bg, k, v;
+        $wrapper = document.createElement('div');
+        $wrapper.id = 'bg';
+        $body.appendChild($wrapper);
 
-    function updateBackgroundImage() {
-        var backgroundImageUrl = settings.imageUrl + '&timestamp=' + new Date().getTime();
-        document.body.style.backgroundImage = 'url("' + backgroundImageUrl + '")';
-    }
+        function updateBackgroundImage() {
+            $bgs[lastPos].classList.remove('top');
+            pos = (pos + 1) % $bgs.length;
+            $bgs[pos].style.backgroundImage = 'url("https://moe.jitsu.top/img/?sort=pc&t=' + new Date().getTime() + '")';
+            $bgs[pos].classList.add('visible');
+            $bgs[pos].classList.add('top');
+            window.setTimeout(function() {
+                $bgs[lastPos].classList.remove('visible');
+            }, settings.delay / 2);
+            lastPos = pos;
+        }
 
-    updateBackgroundImage(); 
+        for (k in settings.images) {
+            $bg = document.createElement('div');
+            $bg.style.backgroundImage = 'url("' + k + '")';
+            $bg.style.backgroundPosition = settings.images[k];
+            $wrapper.appendChild($bg);
+            $bgs.push($bg);
+        }
 
-    setInterval(updateBackgroundImage, settings.delay); 
+        $bgs[pos].classList.add('visible');
+        $bgs[pos].classList.add('top');
+
+        if ($bgs.length == 1 || !canUse('transition')) return;
+
+        window.setInterval(updateBackgroundImage, settings.delay);
+    })();
 })();
