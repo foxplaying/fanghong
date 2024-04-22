d458f248a642f74f6dd5bfc48c23f4b628ec236c
foxplaying <166147707+foxplaying@users.noreply.github.com>
2024-04-23 02:30:19 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,37 +12,35 @@
                 get: i
             }) : t.__defineGetter__(n, i)
         }
-        if (!("undefined" == typeof window.Element || "classList" in document.documentElement)) {
-            var i = Array.prototype,
-                e = i.push,
-                s = i.splice,
-                o = i.join;
-            t.prototype = {
-                add: function(t) {
-                    this.contains(t) || (e.call(this, t), this.el.className = this.toString())
-                },
-                contains: function(t) {
-                    return -1 != this.el.className.indexOf(t)
-                },
-                item: function(t) {
-                    return this[t] || null
-                },
-                remove: function(t) {
-                    if (this.contains(t)) {
-                        for (var n = 0; n < this.length && this[n] != t; n++);
-                        s.call(this, n, 1), this.el.className = this.toString()
-                    }
-                },
-                toString: function() {
-                    return o.call(this, " ")
-                },
-                toggle: function(t) {
-                    return this.contains(t) ? this.remove(t) : this.add(t), this.contains(t)
+        var i = Array.prototype,
+            e = i.push,
+            s = i.splice,
+            o = i.join;
+        t.prototype = {
+            add: function(t) {
+                this.contains(t) || (e.call(this, t), this.el.className = this.toString())
+            },
+            contains: function(t) {
+                return -1 != this.el.className.indexOf(t)
+            },
+            item: function(t) {
+                return this[t] || null
+            },
+            remove: function(t) {
+                if (this.contains(t)) {
+                    for (var n = 0; n < this.length && this[n] != t; n++);
+                    s.call(this, n, 1), this.el.className = this.toString()
                 }
-            }, window.DOMTokenList = t, n(Element.prototype, "classList", function() {
-                return new t(this)
-            })
-        }
+            },
+            toString: function() {
+                return o.call(this, " ")
+            },
+            toggle: function(t) {
+                return this.contains(t) ? this.remove(t) : this.add(t), this.contains(t)
+            }
+        }, window.DOMTokenList = t, n(Element.prototype, "classList", function() {
+            return new t(this)
+        })
     }();
     window.canUse = function(p) {
         if (!window._canUse) window._canUse = document.createElement("div");
@@ -50,12 +48,9 @@
             up = p.charAt(0).toUpperCase() + p.slice(1);
         return p in e || "Moz" + up in e || "Webkit" + up in e || "O" + up in e || "ms" + up in e
     };
-    (function() {
-        if ("addEventListener" in window) return;
-        window.addEventListener = function(type, f) {
-            window.attachEvent("on" + type, f)
-        }
-    })();
+    window.addEventListener = function(type, f) {
+        window.attachEvent("on" + type, f)
+    };
     window.addEventListener('load', function() {
         window.setTimeout(function() {
             $body.classList.remove('is-preload');
