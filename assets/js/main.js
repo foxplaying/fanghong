175fc130b7a8eb15862c7332f4da1fa1502790d6
foxplaying <166147707+foxplaying@users.noreply.github.com>
2024-04-23 03:32:42 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,7 +64,7 @@
 	(function() {
 		var settings = {
 			images: {},
-			delay: 6000
+			delay: 7000
 		};
                 for (var i = 0; i < 10; i++) {
                 settings.images['https://moe.jitsu.top/img/?sort=pc&random=' + Math.random()] = 'center';
