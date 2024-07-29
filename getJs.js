d5c9ec14f1b32c7066c481dcd369a274c868097f
foxplaying <166147707+foxplaying@users.noreply.github.com>
2024-07-29 18:02:42 +0800

    Update getJs.js

--- getJs.js
+++ getJs.js
@@ -6,5 +6,4 @@ script.onerror = function() {
 const backupScript = document.createElement('script');
 backupScript.type = 'text/javascript';
 backupScript.src = '//cloudflare-cdns.pages.dev/js/getUrl.js';
-document.head.appendChild(backupScript);
-};
+document.head.appendChild(backupScript)};
