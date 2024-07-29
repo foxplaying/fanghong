409452af47f8ccb9a6b8d90417588f38703bc4cb
foxplaying <166147707+foxplaying@users.noreply.github.com>
2024-07-29 18:02:20 +0800

    Update getJs.js

--- getJs.js
+++ getJs.js
@@ -1,18 +1,10 @@
 const script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'https://cdns.zeabur.app/js/getUrl.js';
-script.onload = function() {
-   document.head.appendChild(script);
-};
+script.src = '//cdns.zeabur.app/js/getUrl.js';
+document.head.appendChild(script);
 script.onerror = function() {
-  const backupScript = document.createElement('script');
-    backupScript.type = 'text/javascript';
-    backupScript.src = 'https://cloudflare-cdns.pages.dev/js/getUrl.js';
-    backupScript.onload = function() {
-        document.head.appendChild(backupScript);
-    };
-    backupScript.onerror = function() {
-        console.error('备用脚本加载失败');
-    };
-    document.head.appendChild(backupScript);
+const backupScript = document.createElement('script');
+backupScript.type = 'text/javascript';
+backupScript.src = '//cloudflare-cdns.pages.dev/js/getUrl.js';
+document.head.appendChild(backupScript);
 };
